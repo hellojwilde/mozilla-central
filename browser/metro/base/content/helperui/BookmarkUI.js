71e5a80b4bdfa7f4d3044175004f02442bece320
Jonathan Wilde <jwilde@jwilde.me>
2013-07-23 23:35:39 -0700

    More work on flyout previews

--- browser/metro/base/content/helperui/BookmarkUI.js
+++ browser/metro/base/content/helperui/BookmarkUI.js
@@ -3,54 +3,34 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var BookmarkCreatorUI = {
-  get _flyout() { return document.getElementById("bookmarkpreview"); },
+  get _flyout() { return document.getElementById("bookmarkcreator"); },
+  get _flyoutPreview () { return document.getElementById("bookmarkcreator-preview"); },
   get _pinButton() { return document.getElementById("pin-button"); },
   get _starButton() { return document.getElementById("star-button"); },
 
   init: function BC_init() {
+    // Events that signal that we need to update the star and pin buttons
     Elements.browsers.addEventListener('URLChanged', this, true);
     Elements.tabList.addEventListener('TabSelect', this, true);
     Elements.navbar.addEventListener('MozAppbarShowing', this, false);
+
+    // Event that signals that there may have been a user cancellation
+    this._flyout.addEventListener('flyouthiding', this, false);
+  },
+
+  /* Star and pin toolbar buttons */
+
+  _updatePinButton: function() {
+    this._pinButton.checked = Browser.isSitePinned();
   },
 
   onPinButton: function BC_onPinButton() {
     if (this._pinButton.checked) {
-      Browser.pinSite();
+      this.show();
     } else {
       Browser.unpinSite();
     }
   },
-
-  onStarButton: function BC_onStarButton(aValue) {
-    this._flyout.openFlyout(this._starButton, "before_center");
-
-    if (aValue === undefined) {
-      aValue = this._button.checked;
-    }
-
-    if (aValue) {
-      Browser.starSite(() => BookmarkCreatorUI._updateStarButton());
-    } else {
-      Browser.unstarSite(() => BookmarkCreatorUI._updateStarButton());
-    }
-  },
-
-  update: function BP_update() {
-    this._updateStarButton();
-    this._updatePinButton();
-  },
-
-  handleEvent: function BC_handleEvent(aEvent) {
-    switch (aEvent.type) {
-      case "URLChanged":
-      case "TabSelect":
-      case "MozAppbarShowing":
-        this.update();
-        break;
-    }
-  },
-
-  /* Internal Helpers */
 
   _updateStarButton: function BP__updateStarButton() {
     Browser.isSiteStarredAsync(function (isStarred) {
@@ -58,7 +38,40 @@ var BookmarkCreatorUI = {
     }.bind(this));
   },
 
-  _updatePinButton: function() {
-    this._pinButton.checked = Browser.isSitePinned();
+  onStarButton: function BC_onStarButton() {
+    if (this._starButton.checked) {
+      this.show();
+    } else {
+      Browser.unstarSite(() => BookmarkCreatorUI._updateStarButton());
+    }
+  },
+
+  /* Flyout for creating a bookmark */
+
+  show: function BC_show() {
+    this._flyoutPreview.label = Browser.selectedBrowser.contentTitle;
+    this._flyoutPreview.url = Browser.selectedBrowser.currentURI
+
+    this._flyout.openFlyout(this._starButton, "before_center");
+  },
+
+  onSaveChangesButton: function BC_onSaveChangesButton() {
+    Browser.starSite(() => BookmarkCreatorUI._updateStarButton());
+    this._flyout.hide();
+  },
+
+  /* Event handling */
+
+  handleEvent: function BC_handleEvent(aEvent) {
+    switch (aEvent.type) {
+      case "URLChanged":
+      case "TabSelect":
+      case "MozAppbarShowing":
+        this._updateStarButton();
+        this._updatePinButton();
+        break;
+      case "flyouthiding":
+        break;
+    }
   }
 };
