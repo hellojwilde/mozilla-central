61675b752b790d221ad1db28a9ba4fdb530fed86
Jonathan Wilde <jwilde@jwilde.me>
2013-08-14 14:56:16 -0700

    Adding initial highlights UI.

---
+++ browser/metro/base/content/helperui/HighlightsUI.js
@@ -0,0 +1,23 @@
+// -*- Mode: js2; tab-width: 2; indent-tabs-mode: nil; js2-basic-offset: 2; js2-skip-preprocessor-directives: t; -*-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var HighlightsUI = {
+  __menuPopup: null,
+
+  get _panel() { return document.getElementById("highlights-container"); },
+  get _popup() { return document.getElementById("highlights-popup"); },
+
+  get _menuPopup() {
+    if (!this.__menuPopup) {
+      this.__menuPopup = new MenuPopup(this._panel, this._popup);
+      this.__menuPopup.controller = this;
+    }
+    return this.__menuPopup;
+  },
+
+  show: function HUI_show() {
+
+  }
+};
